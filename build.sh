ceda60a69305dc3e2fd4c51ed06f122034815b9c
Yannik Schmidt <sheppy@atlantishq.de>
2019-11-02 13:05:47 +0100

    improve build.sh

--- build.sh
+++ build.sh
@@ -1,15 +1,33 @@
-#cp
-cd sourcemod/scripting/
-cp TrueSkillLoggingMain.sp TrueSkillQueryMain.sp client.sp utils.sp query.sp ~/sourcemod/addons/sourcemod/scripting
+#/bin/bash
 
-# go dir
-cd ~/sourcemod/addons/sourcemod/scripting
+unset LD_PRELOAD
 
-# compile
+if [ $# -ne 1 ]; then
+    echo "Missing Arguments Addons-Dir, see README.md"
+    exit 1
+fi
+
+sourcemod_addons_dir=$1
+plugin_dir=$sourcemod_addons_dir/sourcemod/scripting
+ext_dir=$sourcemod_addons_dir/sourcemod/extensions
+
+cp scripting/TrueSkillLoggingMain.sp \
+   scripting/TrueSkillQueryMain.sp   \
+   scripting/client.sp \
+   scripting/utils.sp  \
+   scripting/query.sp  \
+   $plugin_dir/
+   
+cp scripting/include/* $plugin_dir/include/
+
+cp extensions/* $ext_dir/
+
+cd $plugin_dir
+
 ./compile.sh TrueSkillLoggingMain.sp TrueSkillQueryMain.sp
 
-# copy to plugins
-#cp compiled/insurgency_query.smx compiled/ints_logging.smx ../plugins
+# mv $sourcemod_addons_dir/sourcemod/scripting/TrueSkill* 
+# compiled/insurgency_query.smx compiled/ints_logging.smx ../plugins
 
 # reload
 #insurgency_rcon sm plugins reload insurgency_query.smx && insurgency_small_rcon sm plugins reload insurgency_query.smx
